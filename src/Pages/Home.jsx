4460ce2d8a697ec3cfb19a4cb4c989bd37fa29e9
Sanket Singh <singhsanket143@gmail.com>
2024-06-21 20:14:46 +0530

    Added pointers about app

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,7 +1,8 @@
 import IconArrowRight from "../Components/Icons/ArrowRight";
 
 import PizzaImage from '../assets/Images/pizza2.png';
-
+import CookingImage from '../assets/Images/cooking1.png';
+import IconPatchCheck from "../Components/Icons/IconPatchCheck";
 function Home() {   
     return (
         <div>
@@ -49,6 +50,58 @@ function Home() {
                     />
                 </div>
 
+
+            </section>
+
+
+            {/* Services section */}
+            <section
+                className="py-4 mt-6 bg-gradient-to-r from-amber-50 to-orange-300"
+            >
+                <div className="container flex flex-col md:flex-row">
+                    <div className="flex flex-col items-center justify-center rounded-lg lg:w-1/2">
+                        <img
+                            src={CookingImage}
+                            width={500}
+                            className="rounded-lg"
+                            alt="Cooking"
+                        />
+                    </div>
+                    <div className="flex flex-col flex-wrap text-center lg:py-6 lg:w-1/2 lg:pl-12 lg:text-left">
+
+                        <div className="flex flex-col items-center lg:items-start">
+                            <div>
+                                <h2 className="mb-2 text-5xl font-extrabold text-transparent title-font bg-gradient-to-r from-orange-600 to-orange-300 bg-clip-text">
+                                    Cooked by the best <br/> chefs in the world
+                                </h2>
+                                <p className="text-base leading-relaxed text-[#6B7280]">
+                                    There are many benefits regarding to that but the main ones are:
+                                </p>
+                            </div>
+                        </div>
+
+                        <div className="w-full p-1">
+                            <div className="flex items-center h-full p-2 text-2xl rounded">
+                                <IconPatchCheck className="text-[#F38339] w-10 h-10 mr-4" />
+                                <span className="font-bold title-font">Perfect taste</span>
+                            </div>
+                        </div>
+                        <div className="w-full p-1">
+                            <div className="flex items-center h-full p-2 text-2xl rounded">
+                                <IconPatchCheck className="text-[#F38339] w-10 h-10 mr-4" />
+                                <span className="font-bold title-font">Prepared quickly</span>
+                            </div>
+                        </div>
+                        <div className="w-full p-1">
+                            <div className="flex items-center h-full p-2 text-2xl rounded">
+                                <IconPatchCheck className="text-[#F38339] w-10 h-10 mr-4" />
+                                <span className="font-bold title-font">Food hygeine guaranteed</span>
+                            </div>
+                        </div>
+                        
+                    </div>
+
+                </div>
 
             </section>
 
