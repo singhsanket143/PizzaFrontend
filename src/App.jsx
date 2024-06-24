9cd2d1241763ccc43cbd43c93facee555446dd56
Sanket Singh <singhsanket143@gmail.com>
2024-06-24 19:07:02 +0530

    Added not found page

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import './App.css'
 import Home from './Pages/Home'
 import Signup from './Pages/Auth/Signup'
 import Login from './Pages/Auth/Login'
+import NotFound from './Pages/NotFound'
 
 function App() {
 
@@ -12,6 +13,8 @@ function App() {
         <Route path="/" element={<Home />} />
         <Route path='/auth/signup' element={<Signup />} />
         <Route path='/auth/login' element={<Login />} />
+
+        <Route path='*' element={<NotFound />} />
       </Routes>
     </>
   )
