f817fa71f70b161330e5781651b71683c845548d
Sanket Singh <singhsanket143@gmail.com>
2024-06-24 18:52:44 +0530

    Added signup page

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,17 @@
+import { Route, Routes } from 'react-router-dom'
 import './App.css'
-import Layout from './Layouts/Layout'
 import Home from './Pages/Home'
+import Signup from './Pages/Auth/Signup'
 
 function App() {
 
   return (
-    <div>
-      <Layout>
-        <Home />
-      </Layout>
-    </div>
+    <>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path='/auth/signup' element={<Signup />} />
+      </Routes>
+    </>
   )
 }
 
--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -6,8 +6,10 @@ import IconPatchCheck from "../Components/Icons/IconPatchCheck";
 import OrderFood from '../assets/Images/orderFood.png';
 import Pickup from '../assets/Images/pickup.png';
 import Enjoy from '../assets/Images/enjoy.png';
+import Layout from "../Layouts/Layout";
 function Home() {   
     return (
+        <Layout>
         <div>
             {/* Hero section */}
             <section
@@ -166,7 +168,7 @@ function Home() {
             </section>
 
         </div>
-        
+        </Layout>
     );
 }
 
